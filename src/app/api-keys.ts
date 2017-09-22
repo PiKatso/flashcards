b1290a6a1fbdcc7a57e25236af3e695d27c4f334
Kat Gifford <katgiff2007@gmail.com>
2017-09-22 14:42:47 -0700

    add git ignore api-keys

--- src/app/api-keys.ts
+++
@@ -1,8 +0,0 @@
-export var masterFirebaseConfig = {
-apiKey: "AIzaSyB1pJnjW12FjwxlkntxasSM9QzOp2PLHXw",
-    authDomain: "flashcards-55556.firebaseapp.com",
-    databaseURL: "https://flashcards-55556.firebaseio.comm",
-    projectId: "flashcards-55556",
-    storageBucket: "flashcards-55556.appspot.com",
-    messagingSenderId: "928957330960"
-};    
